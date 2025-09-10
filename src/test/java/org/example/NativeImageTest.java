4f37b3e51306208600041f43d4dcd3966c7f0fe3
Violeta Georgieva <696661+violetagg@users.noreply.github.com>
2025-09-10 18:07:32 +0300

    Add more tests

--- src/test/java/org/example/NativeImageTest.java
+++ src/test/java/org/example/NativeImageTest.java
@@ -1,9 +1,13 @@
 package org.example;
 
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Mono;
 import reactor.netty.DisposableServer;
+import reactor.netty.http.Http11SslContextSpec;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.http.server.HttpServer;
 import reactor.test.StepVerifier;
@@ -12,6 +16,12 @@ import java.time.Duration;
 
 class NativeImageTest {
 	DisposableServer disposableServer;
+	SelfSignedCertificate cert;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		cert = new SelfSignedCertificate();
+	}
 
 	@AfterEach
 	void tearDown() {
@@ -21,15 +31,18 @@ class NativeImageTest {
 	}
 
 	@Test
-	void test() {
+	void test1() {
 		disposableServer =
 				HttpServer.create()
 						.wiretap(true)
+						.secure(spec -> spec.sslContext(Http11SslContextSpec.forServer(cert.certificate(), cert.privateKey())))
 						.handle((req, res) -> res.sendString(Mono.just("Hello World!")))
 						.bindNow();
 
 		HttpClient.create()
 				.port(disposableServer.port())
+				.secure(spec -> spec.sslContext(Http11SslContextSpec.forClient()
+						.configure(builder -> builder.trustManager(InsecureTrustManagerFactory.INSTANCE))))
 				.wiretap(true)
 				.get()
 				.uri("/")
@@ -38,6 +51,21 @@ class NativeImageTest {
 				.asString()
 				.as(StepVerifier::create)
 				.expectNext("Hello World!")
+				.expectComplete()
+				.verify(Duration.ofSeconds(5));
+	}
+
+	@Test
+	void test2() {
+		HttpClient.create()
+				.wiretap(true)
+				.get()
+				.uri("https://projectreactor.io/")
+				.responseContent()
+				.aggregate()
+				.asString()
+				.as(StepVerifier::create)
+				.expectNextMatches(s -> s.contains("Project Reactor"))
 				.expectComplete()
 				.verify(Duration.ofSeconds(5));
 	}
